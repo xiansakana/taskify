46a8801162328fe30523d555a6551e8967a61436
xiansakana <2461298052@qq.com>
2025-02-17 03:20:40 -0500

    update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,12 @@
-export const DATABASE_ID =
-  process.env.NEXT_PUBLIC_APPWITE_DATABASE_ID!;
+export const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID!;
 
-export const WORKSPACES_ID =
-  process.env.NEXT_PUBLIC_APPWITE_WORKSPACES_ID!;
+export const WORKSPACES_ID = process.env.NEXT_PUBLIC_APPWRITE_WORKSPACES_ID!;
 
 export const IMAGES_BUCKET_ID =
-  process.env.NEXT_PUBLIC_APPWITE_IMAGES_BUCKET_ID!;
+  process.env.NEXT_PUBLIC_APPWRITE_IMAGES_BUCKET_ID!;
 
-export const MEMBERS_ID =
-  process.env.NEXT_PUBLIC_APPWITE_MEMBERS_ID!;
+export const MEMBERS_ID = process.env.NEXT_PUBLIC_APPWRITE_MEMBERS_ID!;
 
-export const PROJECTS_ID =
-  process.env.NEXT_PUBLIC_APPWITE_PROJECTS_ID!;
+export const PROJECTS_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECTS_ID!;
 
-export const TASKS_ID =
-  process.env.NEXT_PUBLIC_APPWITE_TASKS_ID!;
+export const TASKS_ID = process.env.NEXT_PUBLIC_APPWRITE_TASKS_ID!;
